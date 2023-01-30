3b56d8ab6c6591b9ebe712f8eec38b2d769b25f0
JoshuaMakuch <joshuamakuch9@gmail.com>
2023-01-30 09:00:31 -0700

    I updated to match the expected formating better

--- Say My Name Again/SayMyNameAgain.vb
+++ Say My Name Again/SayMyNameAgain.vb
@@ -8,19 +8,24 @@ Option Explicit On
 Option Strict On
 
 Imports System
-Imports System.Threading
+Imports System.Threading 'So that Thread.Sleep works to provide a delay
 
 
-Module Program
+Module SayMyNameAgain
     Sub Main(args As String())
+
+        Dim userName As String 'Moved variable initialization
+
         Console.WriteLine("Hello, what is your name?")
-        Dim userName As String = Console.ReadLine()
-        If (userName = "Emily") Or (userName = "Joe") Then
+
+        userName = Console.ReadLine()
+
+        If (userName = "Emily") Or (userName = "Joe") Then                      'These check the user's input to see if it is deserving of a special response
             Console.WriteLine($"Oh! Welcome back {userName}!")
-        ElseIf (userName = "Joshua") Then
+        ElseIf (userName = "Joshua") Then                                       'This outputs a special response to my name
             Console.WriteLine($"Error 404: User {userName} DOES NOT EXIST")
         Else
-            Console.WriteLine("Interesting, I've never heard of you before...")
+            Console.WriteLine("Interesting, I've never heard of you before...") 'This outputs a response for anybody else
             Thread.Sleep(500)
             Console.WriteLine(".")
             Thread.Sleep(500)
@@ -30,5 +35,7 @@ Module Program
             Thread.Sleep(1000)
             Console.WriteLine($"Did you know your name contains the letter {userName.First()}")
         End If
+
     End Sub
+
 End Module
